9fd39d3d895b1eaf146d45c65019f477260e0f40
gudwns1812 <gudwns1812@naver.com>
2025-04-08 16:51:25 +0900

    cal

---
+++ springboot/Calculator/src/test/java/com/study/stringboot/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.stringboot.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
