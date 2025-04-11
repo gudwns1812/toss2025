f5893f8d1b5e2fc5b01a01da1373d7bc3b3bf4bd
gudwns1812 <gudwns1812@naver.com>
2025-04-11 14:40:42 +0900

    inflearn

---
+++ springboot/inflearn/src/test/java/com/study/stringboot/inflearn/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.stringboot.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
