97d98ddffd75499b0787f9f4dcfb6c9c28976b6c
gudwns1812 <gudwns1812@naver.com>
2025-04-21 22:11:30 +0900

    inflearn

---
+++ springboot/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,22 @@
+package hello.core.scan;
+
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = context.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
