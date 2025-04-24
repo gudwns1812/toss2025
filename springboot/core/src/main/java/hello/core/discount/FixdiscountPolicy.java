d61ce9080c5f1a5d9b802bb2d2521a72e5196f28
gudwns1812 <gudwns1812@naver.com>
2025-04-24 13:30:33 +0900

    dd

--- springboot/core/src/main/java/hello/core/discount/FixdiscountPolicy.java
+++ springboot/core/src/main/java/hello/core/discount/FixdiscountPolicy.java
@@ -5,8 +5,6 @@ import hello.core.member.Member;
 
 public class FixdiscountPolicy implements DiscountPolicy {
 
-    private int discountFixAmount = 1000;
-
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
