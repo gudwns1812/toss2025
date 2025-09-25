0dc70cb84090499c11787ed56da56dfd1cee8f1e
gudwns1812 <gudwns1812@naver.com>
2025-09-25 18:47:27 +0900

    buffered

---
+++ java-adv2/src/io/buffered/CreateFIleV3.java
@@ -0,0 +1,25 @@
+package io.buffered;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static io.buffered.BufferedConst.*;
+
+public class CreateFIleV3 {
+    public static void main(String[] args) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(FILE_NAME), BUFFER_SIZE);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < FILE_SIZE; i++) {
+            bos.write(1);
+        }
+        bos.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + " MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+
+    }
+}
