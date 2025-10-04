8f0cbf24c4f7bd136a30da9d6b62419a3eb4d5aa
gudwns1812 <gudwns1812@naver.com>
2025-10-04 23:34:22 +0900

    reflection

---
+++ java-adv2/src/reflection/data/Team.java
@@ -0,0 +1,35 @@
+package reflection.data;
+
+public class Team {
+    private String id;
+    private String name;
+
+    public Team(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
